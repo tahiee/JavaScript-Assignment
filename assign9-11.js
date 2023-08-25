ea96e732913250e4c1fd11b698eabc79f5479fee
tahiee <135806208+tahiee@users.noreply.github.com>
2023-08-25 20:36:00 +0500

    added assig9-11

---
+++ assign9-11.js
@@ -0,0 +1,214 @@
+// 1,question
+// var welcome = prompt('City Name?')
+// if (welcome) {
+//     alert(" Welcome to the city of Lights " + welcome)
+// }
+
+// 2.question
+// var gender = prompt('Your Gender?')
+// if (gender === 'male') {
+//     alert('Good Morning Sir');
+// }
+// else if (gender === 'female') {
+//     alert('Good Morning Maam');
+// } else{
+// alert('Please Enter your Gender');
+// }
+
+// 3.question
+// var color = prompt("Enter any color of traffic signal")
+// if (color.toUpperCase() == "red".toUpperCase()){
+//     alert("MESSAGE IS :"+" Car Vehicles must stop.") 
+// }
+// else if (color.toUpperCase() == "yellow".toUpperCase()) { 
+//     alert("MESSAGE IS :"+" Car should get ready to move.") }
+//  else if (color.toUpperCase() == "green".toUpperCase()) { 
+//     alert("MESSAGE IS :"+" Car can move now.") }
+
+// 4.question
+// var carfuel = prompt('Please input your Car remaining Fuel')
+// var fuel = 0.25
+// if( carfuel < fuel){
+// alert("Please refill your car")
+// } else if ( carfuel > fuel)(
+//     alert('Dear: '+' You can Go' )
+// )
+
+// 5.question
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+//     }
+
+
+// 6.question
+// document.getElementById('total').innerHTML = ('6 Question : Mark sheet')
+// document.getElementById('t300').innerHTML = ('Total Marks : 300')
+// document.getElementById('obt').innerHTML = ('Obtain Marks : 219')
+// var totalmarks = 300
+// var obtmarks = 219
+// var per = obtmarks * 100 / totalmarks
+// document.getElementById('per').innerHTML = `Percentage = ${per} %`
+// if (per >= 60) {
+//     document.getElementById('grade1').innerHTML = ('Grade : B')
+//     document.getElementById('remarks').innerHTML = ('Remarks : You need to Improve')
+//     console.log("Grade B")
+//     console.log("Remarks : You Need to Imporve")
+// }
+
+// 7; question
+// var secret = 44
+// var secret2 = 55
+// var guess = prompt('Guess the Secret Number')
+// console.log(guess, typeof guess)
+// if (guess == secret){
+// alert('Bingo ! Coorect Answer.')
+// }
+// else if (guess == secret2){
+// alert('Bingo ! Correct Answer')
+// }
+// else {
+//     alert('Try Again')
+// }
+
+
+// 8; question
+/*var divnumber = prompt('Write a any dvisibale number')
+if(divnumber % 3 === 0){
+    console.log('divisible number')
+} else {
+    console.log('not a didisiable number')
+}*/
+
+// 9: question
+ /*var number = prompt('Please Write 1 to 12 Check Even Number or Odd Number')
+var even1 = 2
+var even2 = 4
+var even3 = 6
+var even4 = 8
+var even5 = 10
+var even6 = 12
+var odd1 = 3
+var odd2 = 5
+var odd3 = 7
+var odd4 = 9
+var odd5 = 11
+if (number == even1 ){
+    alert('This is a Even Number')
+} 
+if (number == even2  ){
+    alert('This is a Even Number')
+} 
+if (number == even4  ){
+    alert('This is a Even Number')
+} 
+if (number == even5 ){
+    alert('This is a Even Number')
+} 
+if (number == even6 ){
+    alert('This is a Even Number')
+} 
+else if ( number == odd1 ){
+    alert('This is a Odd Number')
+} 
+else if ( number == odd2 ){
+    alert('This is a Odd Number')
+} 
+else if ( number == odd3 ){
+    alert('This is a Odd Number')
+} 
+else if ( number == odd4 ){
+    alert('This is a Odd Number')
+} 
+else if ( number == odd5 ){
+    alert('This is a Odd Number')
+} 
+else{
+    alert('Try Again')
+}
+*/
+
+
+// 10.question
+ /*var t1 = 40
+var t2 = 30
+var t3 = 20
+var t4 = 10
+var temp = prompt('Please Write The Temperature')
+if(t1 == temp){
+alert('“It is too hot outside.”')
+}
+if(t2 == temp){
+alert('“The Weather today is Normal.”')
+}
+if(t3 == temp){
+alert('“Today’s Weather is cool.”')
+}
+if(t4 == temp){
+alert('“OMG! Today’s weather is so Cool.”')
+}
+*/
+
+
+// 11: question
+// hum nan ko check kr sckty hen (nan mean not a number)
+/*var user1Number = +prompt("give me a first number")
+var user2Number = +prompt("give me a second number")
+var operation = prompt("give me a operation(+,/,*,-,%)")
+
+if(isNaN(user1Number) || isNaN(user2Number)) {
+  console.log('not a number')
+} else if (operation === "+"){
+    console.log(user1Number + user2Number)
+} else if (operation === "-"){
+    console.log(user1Number - user2Number)
+} else if (operation === "*"){
+    console.log(user1Number * user2Number)
+} else if (operation === "/"){
+    console.log(user1Number / user2Number)
+} else if (operation === "%"){
+    console.log(user1Number % user2Number)
+} else {
+  console.log('wrong operator')
+}*/
+
+// 9. question 
+/*var number = +prompt("give me a number");
+
+if(number % 2 === 0){
+    console.log('Even number')
+} else {
+    console.log('Odd number')
+}*/
