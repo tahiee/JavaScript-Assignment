6727d7683f9c8e6ececfd488fcdef81dcbe4d1da
tahiee <135806208+tahiee@users.noreply.github.com>
2023-09-13 15:29:11 +0500

    Assignments17-20(js)

---
+++ 17-20.js
@@ -0,0 +1,98 @@
+// 1) question
+
+// var arr = [ ['1','2','3' ['keyborad', 'Mouese']]]
+// var arr2 = []
+// console.log(arry2)
+
+
+
+// 2) question
+
+// var array = [['0','1','2',' 3'],
+// ['1', '0', '1','2'],
+// ['2','1','0','1']]
+
+// console.log(array)
+
+// 3) question
+
+// for (var i = 1; i <= 10; i++){
+// console.log(i)
+// }
+
+
+// 4) question
+// var table = +prompt('Enter a Number to show its Multiples Table')
+// var tablelen = +prompt('Length')
+// for(var i = 1; i <= tablelen; i++){
+// console.log(table + 'x' + i + '='  + table*i)
+// }
+
+// 5) question
+/*
+var fruits = ['apple', 'banana', 'mango', 'orange','strawberry']
+console.log(fruits.slice(0,1))
+console.log(fruits.slice(1,2))
+console.log(fruits.slice(2,3))
+console.log(fruits.slice(3,4))
+console.log(fruits.slice(4,5))
+*/
+
+// 6)question)
+//     for(var i = 1; i <= 15; i++){
+// console.log(i + ',')
+//     }
+
+//     var a = []
+// for(var j = 10; j >= 0; j-=2){
+// a.push(j)
+// console.log(j+',')
+// }
+
+// 7) question
+/*
+var bakkery = ['cookies','apple pie','cake','chips','patties']
+var userinput = prompt('????')
+var index = bakkery.indexOf(userinput)
+if(index === -1){
+    alert('Sorry')
+}else(
+    alert('available at item\n'+ index)
+    )
+    */
+
+// 8) question
+/*
+var A = [24 , 53 , 78 , 91 , 12]
+var userinput =  Number(prompt('input a greater number 24 , 53 , 78 , 91 , 12'))
+var check =  A.find((str)=>str===userinput)
+ if(check === 91){
+alert('this is a largest number is ' + check)
+}else(
+    alert('Sorry Try Again')
+)
+*/
+
+// 9) question
+/*
+var A = [24 , 53 , 78 , 91 , 12]
+var userinput =  Number(prompt('input a Smallest number 24 , 53 , 78 , 91 , 12'))
+var check =  A.find((str)=>str===userinput)
+ if(check === 12){
+alert('this is a Smallest number is ' + check)
+}else(
+    alert('Sorry Try Again')
+)
+*/
+
+
+// 10) question
+/*
+let fiveTableArr = []
+for (var i = 1; i <= 20; i++) {
+    fiveTableArr.push(i * 5)
+    // console.log(i*5)
+}
+console.log(fiveTableArr)
+*/
+
